32b917e53a2040ecac2a61fe87f7bbd1069c7573
rajagp <r7.priya@gmail.com>
2020-01-09 16:52:14 -0500

    Updated install script to point to 2.6.3 version of CBL

--- content/modules/userprofile/examples/install_11.sh
+++ content/modules/userprofile/examples/install_11.sh
@@ -4,7 +4,7 @@ cd Frameworks
 rm -rf iOS
 rm -rf macOS
 rm -rf tvOS
-curl http://packages.couchbase.com/releases/couchbase-lite-ios/2.6.1/couchbase-lite-swift_enterprise_2.6.1.zip > cbl.zip
+curl http://packages.couchbase.com/releases/couchbase-lite-ios/2.6.3/couchbase-lite-swift_enterprise_2.6.3.zip > cbl.zip
 unzip -n cbl.zip
 rm -rf cbl.zip
 rm -rf cbl
