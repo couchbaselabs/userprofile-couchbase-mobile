2348b1e57556b3657298e61ada3ec2dcb422f115
rajagp <r7.priya@gmail.com>
2020-02-21 12:26:58 -0500

    Updated instructions to point to CBL 2.7

--- content/modules/userprofile/examples/install_11.sh
+++ content/modules/userprofile/examples/install_11.sh
@@ -4,7 +4,7 @@ cd Frameworks
 rm -rf iOS
 rm -rf macOS
 rm -rf tvOS
-curl http://packages.couchbase.com/releases/couchbase-lite-ios/2.6.1/couchbase-lite-swift_enterprise_2.6.1.zip > cbl.zip
+curl http://packages.couchbase.com/releases/couchbase-lite-ios/2.7.0/couchbase-lite-swift_enterprise_2.7.0.zip > cbl.zip
 unzip -n cbl.zip
 rm -rf cbl.zip
 rm -rf cbl
