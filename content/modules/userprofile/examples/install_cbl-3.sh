0216bd822944f0740fda2f61bedbc00b048abf77
jayahariv <10448770+jayahariv@users.noreply.github.com>
2022-01-13 00:43:02 -0800

    update script to use from relative path update to use XCFramework

--- content/modules/userprofile/examples/install_cbl-3.sh
+++ content/modules/userprofile/examples/install_cbl-3.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-cd Frameworks
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+pushd $PARENT_PATH
+
 rm -rf iOS
 rm -rf macOS
 rm -rf tvOS
-curl http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-ios/3.0.0/210/couchbase-lite-swift_enterprise_3.0.0-210.zip > cbl.zip
+curl http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-ios/3.0.0/345/couchbase-lite-swift_xc_enterprise_3.0.0-345.zip > cbl.zip
 unzip -n cbl.zip
 rm -rf cbl.zip
 rm -rf cbl
+
+popd
+
