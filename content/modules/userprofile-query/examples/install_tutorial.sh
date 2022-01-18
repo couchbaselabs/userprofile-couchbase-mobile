3f90ca4432966b89d378006510aab330db786929
jayahariv <10448770+jayahariv@users.noreply.github.com>
2022-01-17 23:14:40 -0800

    udpate the script

--- content/modules/userprofile-query/examples/install_tutorial.sh
+++ content/modules/userprofile-query/examples/install_tutorial.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
-  echo num pars = $#
-  if [ $# -eq 0 ]
+
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+echo num pars = $#
+if [ $# -eq 0 ]
+then
+  echo "undefined release number"
+else
+  if [ $# = 1 ]
   then
-    echo "undefined release number"
+    target="cbl.zip"
+    release="https://packages.couchbase.com/releases/couchbase-lite-ios/"$1"/couchbase-lite-swift_enterprise_"$1".zip"
+    echo Retrieving $release
+    echo Creating $target
+    
+    pushd $PARENT_PATH
+    
+    rm -rf Frameworks
+    mkdir -p Frameworks
+    cd Frameworks
+    
+    rm -rf iOS
+    rm -rf macOS
+    rm -rf tvOS
+    curl $release > $target
+    unzip -n $target
+    rm -rf $target
+    rm -rf cbl
+    
+    popd
   else
-    if [ $# = 1 ]
-    then
-      target="cbl.zip"
-      release="https://packages.couchbase.com/releases/couchbase-lite-ios/"$1"/couchbase-lite-swift_enterprise_"$1".zip"
-      echo Retrieving $release
-      echo Creating $target
-      cd Frameworks
-      rm -rf iOS
-      rm -rf macOS
-      rm -rf tvOS
-      curl $release > $target
-      unzip -n $target
-      rm -rf $target
-      rm -rf cbl
-    else
-      echo "too many parameters provided -- just need release number of package to download"
-    fi
+    echo "too many parameters provided -- just need release number of package to download"
   fi
+fi
